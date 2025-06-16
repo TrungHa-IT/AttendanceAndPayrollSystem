09664a9bb2577a127b8289934275210ebc254212
Le Duy Hai <haileduy72@gmail.com>
2025-06-16 20:20:53 +0700

    ddone config

---
+++ FUNAttendanceAndPayrollSystemAPI/DTO/EmployeeDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace FUNAttendanceAndPayrollSystemAPI.DTO
+{
+    public class EmployeeDTO
+    {
+        public int EmployId { get; set; }
+
+        public string? EmployeeName { get; set; }
+
+        public DateOnly? Dob { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Gender { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Position { get; set; }
+
+        public string Status { get; set; } = null!;
+    }
+}
