3e1d4235de6106fd4513d06954101bf96a06de34
TrungHa-IT <hatrung03022003@gmail.com>
2025-06-24 01:22:22 +0700

    add leaveDTO and leaveDAO

---
+++ DataTransferObject/LeaveDTO/LeaveDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTransferObject.LeaveDTO
+{
+    public class LeaveDTO
+    {
+        public int LeaveId { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public decimal? DurationInDays { get; set; }
+
+        public string? Reason { get; set; }
+
+        public string? Status { get; set; }
+
+        public int? ApprovedBy { get; set; }
+
+    }
+}
